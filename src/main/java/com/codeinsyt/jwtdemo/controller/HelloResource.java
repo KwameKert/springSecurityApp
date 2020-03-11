e3abd6dc5750443f4f14455ec4e4a3e1afbdbfef
kwamekert <tomriddle477@gmail.com>
2020-03-11 16:04:04 +0000

    Models added

--- src/main/java/com/codeinsyt/jwtdemo/controller/HelloResource.java
+++ src/main/java/com/codeinsyt/jwtdemo/controller/HelloResource.java
@@ -1,14 +1,28 @@
 package com.codeinsyt.jwtdemo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloResource {
+//    @Autowired
+//    private AuthenticationManager authenticationManager;
 
     @RequestMapping("/hello")
     public String hello(){
         return "Hello World";
     }
+
+//    @PostMapping("/authenticate")
+//    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception{
+//
+//    }
+
+
 }
